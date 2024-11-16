33d0449966ac4609a1f30ef0e13ae16e1a6bdb03
akkiB <akash3350saha@gmail.com>
2024-11-16 21:34:46 +0530

    infix

--- Function/functions2.kt
+++ Function/functions2.kt
@@ -1,4 +1,6 @@
-
+/*
+Extension Function -> "This" refers to current variable context
+ */
 fun String.reverseF():String{
 
     if(this.length==1){
@@ -15,10 +17,23 @@ fun String.reverseF():String{
     return result.toString().lowercase()
 }
 
-fun Int.isGreaterThan(num:Int) :Boolean = this > num 
+fun Int.isGreaterThan(num:Int) :Boolean = this > num
+
+class Header(val name:String)
+
+infix fun Header.plus(other:Header):Header{
+return Header(this.name+other.name)
+}
 
 fun main(){
     val rev  =  "Akash".reverseF()
     println("$rev")
     println(5.isGreaterThan(3))
+
+    val h1 = Header("AKASH ")
+    val h2 = Header("SAHA")
+
+    val result = h1 plus h2
+
+    println(result.name)
 }
