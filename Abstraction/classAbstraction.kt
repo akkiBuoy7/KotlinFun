136d51166ad68bda9b763a97095e198ffe974971
akkiB <akash3350saha@gmail.com>
2024-11-17 22:53:23 +0530

    abstraction class

---
+++ Abstraction/classAbstraction.kt
@@ -0,0 +1,22 @@
+abstract class Calculator(){
+    abstract fun calc(x:Int,y:Int):Int
+}
+
+class Multiple:Calculator(){
+override fun calc(x:Int,y:Int):Int{
+return x*y
+}
+}
+
+class Add:Calculator(){
+    override fun calc(x:Int,y:Int):Int{
+    return x+y
+    }
+    }
+
+fun main(){
+    val obj = Multiple()
+    println(obj.calc(3, 6))
+    val obj1 = Add()
+    println(obj1.calc(9, 5))
+}
